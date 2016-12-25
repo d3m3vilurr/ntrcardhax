a1832a527b6ad4da14824d022058408dfb264f39
Sunguk Lee <d3m3vilurr@gmail.com>
2016-12-25 14:50:18 +0900

    Add masking rule
    script wrong returned with n3ds native firms

--- auto_ntrcardhax/scripts/find_addr.py
+++ auto_ntrcardhax/scripts/find_addr.py
@@ -1,10 +1,15 @@
 import sys
 import struct
 
-def search(binary, pattern, skip=0):
+def search(binary, pattern, skip=0, masks=None):
     pattern_len = len(pattern)
-    for idx in xrange(len(binary)):
-        if binary[idx : idx + pattern_len] != pattern:
+    for idx in xrange(len(binary) - pattern_len):
+        b = binary[idx : idx + pattern_len]
+        if masks:
+            for offset, maskbit in masks:
+                target_uint = struct.unpack('I', (b[offset:offset + 4]))[0]
+                b = b[:offset] + struct.pack('I', target_uint & maskbit) + b[offset + 4:]
+        if b != pattern:
             continue
         return struct.unpack('I', (binary[idx + skip: idx + skip + 4]))[0]
 
@@ -44,10 +49,12 @@ def exist_map_memory_adr(arm9bin):
     # F0 40 2D E9         STMFD   SP!, {R4-R7,LR}
     # 14 D0 4D E2         SUB     SP, SP, #0x14
     # 03 40 A0 E1         MOV     R4, R3
-    # 7C C0 9F E5         LDR     R12, =0x1F600000
+    # 7C C0 9F E5         LDR     R12, =0x1F600000 ; mask ~0xFFF
     # 28 50 9D E5         LDR     R5, [SP,#0x28]
     type0 = search(arm9bin,
-                   '\xf0\x40\x2d\xe9\x14\xd0\x4d\xe2\x03\x40\xa0\xe1\x7c\xc0\x9f\xe5\x28\x50\x9d\xe5')
+                   '\xf0\x40\x2d\xe9\x14\xd0\x4d\xe2\x03\x40\xa0\xe1\x00\xc0\x9f\xe5'
+                   '\x28\x50\x9d\xe5',
+                   masks=[(0x0C, ~0xFFF)])
     # FF 5F 2D E9         STMFD   SP!, {R0-R12,LR}
     # 1F 04 51 E3         CMP     R1, #0x1F000000
     # 01 40 A0 E1         MOV     R4, R1
@@ -55,7 +62,8 @@ def exist_map_memory_adr(arm9bin):
     # 00 80 A0 E1         MOV     R8, R0
     # 05 26 81 E0         ADD     R2, R1, R5,LSL#12
     type1 = search(arm9bin,
-                   '\xff\x5f\x2d\xe9\x1f\x04\x51\xe3\x01\x40\xa0\xe1\x02\x50\xa0\xe1\x00\x80\xa0\xe1\x05\x26\x81\xe0')
+                   '\xff\x5f\x2d\xe9\x1f\x04\x51\xe3\x01\x40\xa0\xe1\x02\x50\xa0\xe1'
+                   '\x00\x80\xa0\xe1\x05\x26\x81\xe0')
 
     # 7F 40 2D E9         STMFD   SP!, {R0-R6,LR}
     # 03 C0 A0 E1         MOV     R12, R3
@@ -63,7 +71,8 @@ def exist_map_memory_adr(arm9bin):
     # 02 36 81 E0         ADD     R3, R1, R2, LSL#12
     # 20 E0 9D E5         LDR     LR, [SP, #0x20]
     type2 = search(arm9bin,
-                   '\x7f\x40\x2d\xe9\x03\xc0\xa0\xe1\x7a\x05\x51\xe3\x02\x36\x81\xe0\x20\xe0\x9d\xe5')
+                   '\x7f\x40\x2d\xe9\x03\xc0\xa0\xe1\x7a\x05\x51\xe3\x02\x36\x81\xe0'
+                   '\x20\xe0\x9d\xe5')
     if type0 or type1 or type2:
         return 'true'
     return 'false'
